a9c3f0c7b0e9a156cae8c701203d3d83445060fa
a-jumani <arsalan.jumani@outlook.com>
2020-06-16 11:14:11 +0400

    feat: Add checkpointing implementation

--- checkpoint.py
+++ checkpoint.py
@@ -1,3 +1,14 @@
+import os
+import shutil
+
+
+class CHECKPOINT_STATE(object):
+    _RAND_NUM = '1247821013431315'
+    IN_PROG = '_cp_ip' + _RAND_NUM
+    OLD = '_cp_old' + _RAND_NUM
+    CURRENT = '_cp' + _RAND_NUM
+
+
 def create_checkpoint(path: str, empty: bool = False):
     """ Create copy of file(s) at path or an empty directory as a checkpoint.
     Atomic from the perspective of the caller.
@@ -9,7 +20,22 @@ def create_checkpoint(path: str, empty: bool = False):
     Preconditions:
         file(s) at path should not be changing while this function executes
     """
-    pass
+    if empty:
+        # empty checkpoint
+        os.mkdir(path + CHECKPOINT_STATE.IN_PROG)
+    else:
+        # copy contents
+        shutil.copytree(path, path + CHECKPOINT_STATE.IN_PROG)
+
+    # rename current checkpoint to old checkpoint
+    if os.path.isdir(path + CHECKPOINT_STATE.CURRENT):
+        os.rename(path + CHECKPOINT_STATE.CURRENT, path + CHECKPOINT_STATE.OLD)
+
+    # rename copied contents to current checkpoint
+    os.rename(path + CHECKPOINT_STATE.IN_PROG, path + CHECKPOINT_STATE.CURRENT)
+
+    # remove old checkpoint
+    _remove_directory(path + CHECKPOINT_STATE.OLD)
 
 
 def restore_checkpoint(path: str):
@@ -19,7 +45,27 @@ def restore_checkpoint(path: str):
     Args:
         path            path to a directory
     """
-    pass
+    # current checkpoint exists
+    if os.path.isdir(path + CHECKPOINT_STATE.CURRENT):
+        shutil.rmtree(path)
+        shutil.copytree(path + CHECKPOINT_STATE.CURRENT, path)
+
+    # old checkpoint exists - checkpoint creation failed after copying
+    # contents successfully
+    elif os.path.isdir(path + CHECKPOINT_STATE.OLD):
+        os.rename(path + CHECKPOINT_STATE.IN_PROG,
+                  path + CHECKPOINT_STATE.CURRENT)
+        shutil.rmtree(path)
+        shutil.copytree(path + CHECKPOINT_STATE.CURRENT, path)
+
+    # no checkpoints
+    else:
+        _remove_directory(path)
+        os.mkdir(path)
+
+    # clean up - old and incomplete checkpoints
+    _remove_directory(path + CHECKPOINT_STATE.OLD)
+    _remove_directory(path + CHECKPOINT_STATE.IN_PROG)
 
 
 def clear_checkpoint(path: str):
@@ -29,4 +75,11 @@ def clear_checkpoint(path: str):
     Args:
         path            path to a directory
     """
-    pass
+    create_checkpoint(path, empty=True)
+    os.rmdir(path + CHECKPOINT_STATE.CURRENT)
+
+
+# helper functions
+def _remove_directory(path):
+    if os.path.isdir(path):
+        shutil.rmtree(path)
